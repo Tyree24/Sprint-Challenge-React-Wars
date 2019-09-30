3151973a1ccbac9af05ca6bed6901c49f28c91cd
Tyree LeBeau <tyreelebeau24@gmail.com>
2019-09-29 22:31:28 -0500

    completed mvp

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -19,6 +19,8 @@ const App = () => {
   // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  
+  //fetching data
   useEffect(() => {
     axios
       .get("https://swapi.co/api/people/")
@@ -47,3 +49,4 @@ const App = () => {
 };
 
 export default App;
+
